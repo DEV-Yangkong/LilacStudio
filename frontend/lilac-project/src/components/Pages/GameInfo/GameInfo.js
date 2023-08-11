239b1e29bc4933438c2641af6bc346d9032aa48e
yangkong <dev.yangkong@gmail.com>
2023-08-11 14:25:24 +0900

    [FIX] CSS 모듈 문제 해결

--- frontend/lilac-project/src/components/Pages/GameInfo/GameInfo.js
+++ frontend/lilac-project/src/components/Pages/GameInfo/GameInfo.js
@@ -3,9 +3,11 @@ import styles from "./Game.module.css";
 
 const GameInfo = () => {
   return (
-    <section className="game-info">
-      <h2>게임 정보</h2>
-      <p>다가오는 게임에 대한 소개와 정보를 확인하세요.</p>
+    <section className={styles.gameInfo}>
+      <h2 className={styles.gameInfoTitle}>게임 정보</h2>
+      <p className={styles.gameInfoText}>
+        다가오는 게임에 대한 소개와 정보를 확인하세요.
+      </p>
     </section>
   );
 };
--- frontend/lilac-project/src/components/Pages/Media/Media.js
+++ frontend/lilac-project/src/components/Pages/Media/Media.js
@@ -3,8 +3,8 @@ import styles from "./Media.module.css";
 
 const Media = () => {
   return (
-    <section className="media">
-      <h2>미디어</h2>
+    <section className={styles.media}>
+      <h2 className={styles.heading}>미디어</h2>
       <p>게임과 관련된 이미지와 동영상을 살펴보세요.</p>
     </section>
   );
