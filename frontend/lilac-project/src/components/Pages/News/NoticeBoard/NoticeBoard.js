ac57db6139eb65cff17075fc30a79d2c15ce42cc
yangkong <dev.yangkong@gmail.com>
2023-08-16 10:51:25 +0900

    [UPDATE] 공지사항 CSS,코드 수정

--- frontend/lilac-project/src/components/Pages/News/NoticeBoard/NoticeBoard.js
+++ frontend/lilac-project/src/components/Pages/News/NoticeBoard/NoticeBoard.js
@@ -2,27 +2,44 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import styles from "./NoticeBoard.module.css";
+import "font-awesome/css/font-awesome.min.css";
 
 const NoticeBoard = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [scrollButtonVisible, setScrollButtonVisible] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [noticePosts, setNoticePosts] = useState([]);
+  const [youtubePosts, setYoutubePosts] = useState([]);
 
-  const postsPerPage = 6; // 페이지당 게시글 수
+  const postsPerPage = 9;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "백엔드 API 주소" // 백엔드에서 게시글 데이터를 받아오는 API 주소
+          "http://127.0.0.1:8000/api/v1/notice_board/"
         );
-        setNoticePosts(response.data);
+        setYoutubePosts(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 130) {
+        setScrollButtonVisible(true);
+      } else {
+        setScrollButtonVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   const handlePageChange = (newPage) => {
@@ -37,7 +54,13 @@ const NoticeBoard = () => {
     });
   };
 
-  const filteredPosts = noticePosts.filter(
+  const formatDate = (dateString) => {
+    const isoDateString = dateString; // 예: '2023-08-08T11:59:01.894580+09:00'
+    const formattedDateString = isoDateString.split("T")[0]; // '2023-08-08'
+    return formattedDateString.replace(/\./g, "-"); // '.'을 '-'로 변경
+  };
+
+  const filteredPosts = NoticeBoard.filter(
     (post) =>
       searchTerm === "" ||
       post.title.toLowerCase().includes(searchTerm.toLowerCase())
@@ -47,30 +70,79 @@ const NoticeBoard = () => {
 
   const startIndex = (currentPage - 1) * postsPerPage;
   const endIndex = Math.min(startIndex + postsPerPage, filteredPosts.length);
-  const postsToShow = filteredPosts.slice(startIndex, endIndex);
-
-  const formatDate = (dateString) => {
-    // 날짜 포맷팅 함수 구현
-  };
+  const reversedFilteredPosts = [...filteredPosts].reverse();
+  const postsToShow = reversedFilteredPosts.slice(startIndex, endIndex);
 
   return (
-    <div className={styles["noticeBoard"]}>
+    <div className={styles["notice-board"]}>
       <h1>공지사항</h1>
-      <div className={styles["search-bar"]}>{/* 검색창 코드 추가 */}</div>
+      <div className={styles["search-bar"]}>
+        <input
+          type="text"
+          placeholder="제목 검색"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <span className={styles["search-icon"]}>
+          <i className="fa fa-search"></i>
+        </span>
+      </div>
+      {searchTerm && (
+        <div className={styles["search-result"]}>
+          <p>검색 결과: "{searchTerm}"</p>
+        </div>
+      )}
       {filteredPosts.length === 0 && searchTerm !== "" ? (
         <div className={styles["no-results"]}>검색 결과가 없습니다.</div>
       ) : (
         <div className={styles["post-list"]}>
-          {/* 게시글 목록 매핑하여 표시 */}
+          {postsToShow.map((post) => (
+            <div className={styles["post-item"]} key={post.id}>
+              <Link
+                to={`/youtube/${post.id}`}
+                className={styles["post-title-link"]}
+              >
+                <img src={post.thumbnail_url} alt={post.title} />
+                <div className={styles["post-title"]}>{post.title}</div>
+              </Link>
+              <div className={styles["post-info"]}>
+                <span className={styles["post-date"]}>
+                  {formatDate(post.created_at)}
+                </span>
+                <span className={styles["post-views"]}>
+                  조회수 {post.views_count}
+                </span>
+              </div>
+            </div>
+          ))}
         </div>
       )}
-      {/* 페이지 네비게이션 추가 */}
+      {scrollButtonVisible && (
+        <button className={styles["top-button"]} onClick={scrollToTop}>
+          TOP
+        </button>
+      )}
+      {totalPageCount > 1 && (
+        <div className={styles["page-navigation"]}>
+          {Array.from({ length: totalPageCount }, (_, i) => i + 1).map(
+            (page) => (
+              <button
+                key={page}
+                className={page === currentPage ? styles["active"] : ""}
+                onClick={() => handlePageChange(page)}
+              >
+                {page}
+              </button>
+            )
+          )}
+        </div>
+      )}
       <div className={styles["write-button-container"]}>
         <Link
           to="/news/notice-board/write-post-nb"
           className={styles["write-button"]}
         >
-          글쓰기
+          UPLOAD
         </Link>
       </div>
     </div>
