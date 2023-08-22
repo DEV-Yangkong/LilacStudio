fc188f54e86d5dd82959d7704035b0a3547e12bf
yangkong <dev.yangkong@gmail.com>
2023-08-22 14:59:40 +0900

    db삭제전 임시저장 커밋

--- backend/youtube/models.py
+++ backend/youtube/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class YouTubePost(models.Model):
     title = models.CharField(max_length=200)
+    content = models.TextField()
     video_url = models.URLField(max_length=800, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     views_count = models.PositiveIntegerField(default=0)
